c6c9e7fdfc50e80d023906c1e5b650055863ed78
Montalvas <lucas_montalvani@hotmail.com>
2021-06-01 13:49:13 -0300

    update

--- exercises/ex003.py
+++ exercises/ex003.py
@@ -1,3 +1,5 @@
+#Leia dois números e mostre a soma entre eles:
+
 n1 = int(input('Primeiro número: '))
 n2 = int(input('Segundo número: '))
 print('A soma entre {} e {} = {soma}'.format(n1, n2, soma = n1 + n2))
---
+++ exercises/ex004.py
@@ -0,0 +1,12 @@
+# Leia algo digitado, diga o tipo primitivo e as possívesi informações
+
+value = input('Digite algo: ')
+print('O tipo primitivo de {} é: {}'.format(value, type(value)))
+print('É numérico: {}'.format(value.isnumeric()))
+print('É alfabético: {}'.format(value.isalpha()))
+print('É alfanumérico: {}'.format(value.isalnum()))
+print('É decimal: {}'.format(value.isdecimal()))
+print('É composto por espaços em branco: {}'.format(value.isspace()))
+print('É composto por dígitos: {}'.format(value.isdigit()))
+print('Está todo em maiúsculo: {}'.format(value.isupper()))
+print('Está todo em minúsculo: {}'.format(value.islower()))
