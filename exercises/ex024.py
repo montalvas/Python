251093040d074aa385900d68b032f693d64f2d8d
Montalvas <62609007+montalvas@users.noreply.github.com>
2021-06-09 19:02:28 -0300

    String
    ex024 - ex027

---
+++ exercises/ex024.py
@@ -0,0 +1,5 @@
+# Ler o nome de uma cidade e dizer se começa com "SANTO" ou não
+
+cidade = input('Informe uma cidade: ')
+cidade = cidade.lower()
+print('O nome da cidade começa com Santo: ', cidade.startswith("santo"))
