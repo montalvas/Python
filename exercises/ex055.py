1a43ca416bd8761dc6847eee84d6797a49463eae
Montalvas <lucas_montalvani@hotmail.com>
2021-06-19 09:38:04 -0300

    Update ex055.py

--- exercises/ex055.py
+++ exercises/ex055.py
@@ -4,11 +4,12 @@ maior = 0
 menor = 0
 for c in range(0, 5):
     peso = float(input('Digite seu peso: '))
-    if peso > maior:
-        maior = peso
     if c == 0:
+        maior = peso
         menor = peso
     else:
+        if peso > maior:
+            maior = peso
         if peso < menor:
             menor = peso
 print('O maior peso foi: {} KG'.format(maior))
