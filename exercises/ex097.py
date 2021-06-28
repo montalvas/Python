4d7e8cf110bfd6abf16a2255d7d919f0fac76108
Montalvas <lucas_montalvani@hotmail.com>
2021-06-28 18:38:21 -0300

    Functions
    ex096 - ex097

---
+++ exercises/ex097.py
@@ -0,0 +1,10 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print('  ' + msg + '  ')
+    print('~' * tam)
+
+
+escreva('Olá, mundo!')
+escreva('Curso em vídeo')
+escreva('Curso de Python no Youtube')
